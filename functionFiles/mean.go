c2674afd91774d6b02d270c9a08b5943ef04002f
shfana <wambitafana@gmail.com>
2024-08-10 00:11:15 +0300

    Test: add test for mean function

--- functionFiles/mean.go
+++ functionFiles/mean.go
@@ -1,11 +1,12 @@
 package functionFiles
 
+// calculates the mean of a  slice of float numbers
 func Mean(numslice []float64) float64 {
 	sum := 0.0
-	lenNum := len(numslice)
+	sliceLength := len(numslice)
 	for _, num := range numslice {
 		sum += num
 	}
-	mean := sum / float64(lenNum)
+	mean := sum / float64(sliceLength)
 	return mean
 }
